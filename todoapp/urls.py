da5da95247475f84f6591a6665e1933c63f5f989
nicolesy <nicole@nicolesy.com>
2019-10-30 14:40:45 -0700

    Updated the todo app for Django (everything except the "extra_text" part of the model).

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -6,8 +6,13 @@ from . import views
 app_name = 'todoapp'
 urlpatterns = [
     path('index/', views.index, name='index'),
+    path('new_todo/', views.new_todo, name='new_todo'),
+    path('edit_todo/<int:todo_id>/', views.edit_todo, name='edit_todo'),
+    path('update_todo/<int:todo_id>/', views.update_todo, name='update_todo'),
     path('save_todo/', views.save_todo, name='save_todo'),
-    path('complete_todo/', views.complete_todo, name='complete_todo')
+    path('complete_todo/', views.complete_todo, name='complete_todo'),
+    path('delete_todo/', views.delete_todo, name='delete_todo'),
+    path('delete_all/', views.delete_all, name='delete_all'),
 ]
 
 
--- todoapp/views.py
+++ todoapp/views.py
@@ -7,9 +7,10 @@ from .models import TodoItem, TodoItemType
 def index(request):
     todo_items = TodoItem.objects.order_by('date_completed', 'date_created')
     todo_types = TodoItemType.objects.order_by('name')
+    
     context = {
         'todo_items': todo_items,
-        'todo_types': todo_types
+        'todo_types': todo_types, 
     }
     return render(request, 'todoapp/index.html', context)
 
@@ -39,4 +40,64 @@ def complete_todo(request):
     todo_item.date_completed = timezone.now()
     todo_item.save()
     return HttpResponseRedirect(reverse('todoapp:index'))
+
+
+def delete_todo(request):
+    todo_item_delete = request.POST['todo_item_delete']
+    todo_item = TodoItem.objects.get(id=todo_item_delete)
+    todo_item.delete()
+    return HttpResponseRedirect(reverse('todoapp:index'))
     
+
+def delete_all(request):
+    todo_item_delete_all = request.POST['todo_item_delete_all']
+    todo_item = TodoItem.objects.all()
+    todo_item.delete()
+    return HttpResponseRedirect(reverse('todoapp:index'))
+    
+
+def new_todo(request):
+    todo_types = TodoItemType.objects.order_by('name')
+    
+    context = {
+        'todo_types': todo_types, 
+    }
+    return render(request, 'todoapp/new_todo.html', context)
+    
+
+def edit_todo(request, todo_id):
+    todo_item = TodoItem.objects.get(id=todo_id)
+    
+    todo_types = TodoItemType.objects.order_by('name')
+    
+    context = {
+        "todo_item": todo_item,
+        "todo_types": todo_types,
+    }
+    
+    return render(request, 'todoapp/edit_todo.html', context)
+
+
+def update_todo(request, todo_id):
+    
+    todo_item = TodoItem.objects.get(id=todo_id)
+    
+    # get variables out of the form data
+    todo_text = request.POST['todo_text']
+    todo_type_ids = request.POST.getlist('todo_type_ids')
+    
+    todo_item.text = todo_text
+    todo_item.save()
+    
+    todo_item.types.clear()
+    
+    # create many-to-many relationship with types
+    for todo_type_id in todo_type_ids:
+        todo_type = TodoItemType.objects.get(id=todo_type_id)
+        todo_item.types.add(todo_type)
+        
+    
+    
+    
+    # redirect to the index page
+    return HttpResponseRedirect(reverse('todoapp:index'))
